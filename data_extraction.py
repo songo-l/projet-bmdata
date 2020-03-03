f8e98ac1a35d0475449be3cc98c8edfa45f7b6e7
songo <songo@localhost.localdomain>
2020-03-03 15:56:26 +0100

    petit changement du data_extraction

--- data_extraction.py
+++ data_extraction.py
@@ -2,11 +2,11 @@ import tensorflow as tf
 
 MAKEUP_DIR='all_images/all_images/'
 
-def create_datasets(path):
+def create_datasets(path, batch_size=32):
     images_gen = tf.keras.preprocessing.image.ImageDataGenerator(rescale = 1./255, horizontal_flip=True)
     images = images_gen.flow_from_directory(
         path, target_size=(256, 256), color_mode='rgb', classes=None,
-        class_mode='categorical', batch_size=32, shuffle=False, seed=None,
+        class_mode='categorical', batch_size=batch_size, shuffle=False, seed=None,
         save_to_dir=None, save_prefix='', save_format='png', follow_links=False,
         subset=None, interpolation='nearest'
         )
