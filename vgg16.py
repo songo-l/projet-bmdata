2a9fbfe462171bd4a9218b52936a49339f5b3f31
songo <songo@localhost.localdomain>
2020-03-05 11:30:23 +0100

    dependances vgg16

--- vgg16.py
+++ vgg16.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-
+from tensorflow.keras.applications import vgg16
 def prec_loss_cal(input_tensor):
     initial_model = vgg16.VGG16(input_tensor=input_tensor, weights="imagenet", include_top=False)
     initial_model.trainable=False
